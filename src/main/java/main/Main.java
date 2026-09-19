cb6f3c8c9ab44edc4d0c27a731da5faa2edb7667
NightHvnter183M <nikita.bednov.2000@gmail.com>
2026-09-19 09:30:12 +0300

    Initial commit

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,40 @@
+package main;
+
+import arc.util.CommandHandler;
+import mindustry.gen.Player;
+import mindustry.mod.Plugin;
+
+public class Main extends Plugin {
+
+    ServerManager serverManager;
+
+    @Override
+    public void init(){
+        ///Here will be admin settings of the lobby
+    serverManager = new ServerManager();
+    Resources.currentDuels.clear();
+    Resources.duelRequests.clear();
+
+
+
+
+    }
+    ///And here are commands
+    public void registerClientCommands(CommandHandler handler){
+        handler.<Player>register("duel", "Call a player on a duel", (args, player) -> {
+            ///here will be logic of this shit
+        });
+        handler.<Player>register("accept", "accept a duel", (args, player) -> {
+
+        });
+        handler.<Player>register("deny", "deny a duel", (args, player) ->{
+
+        });
+
+        handler.<Player>register("spectate", "watch other people plaing duels", (args, player) -> {
+
+        });
+    }
+
+
+}
---
+++ src/main/java/main/Resources.java
@@ -0,0 +1,8 @@
+package main;
+
+import arc.struct.ObjectMap;
+
+public class Resources {
+    static ObjectMap<String, String> duelRequests = new ObjectMap<>();
+    static ObjectMap<Integer, String> currentDuels = new ObjectMap<>();
+}
